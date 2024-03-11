ebb0ff40720a29f266b4873698644613bb478cc5
gawxgd <128865159+gawxgd@users.noreply.github.com>
2024-03-11 13:58:34 +0100

    gdi, pick color dialog, help (no hotkeys), systemtray (not displaying)

--- lab1pige/app.cpp
+++ lab1pige/app.cpp
@@ -7,7 +7,7 @@
 #define ID_TIMER2 2
 #define F4 2
 #define ALTC 3
-
+#define F12 4
 std::wstring const app::s_class_name{ L"app window" };
 std::wstring const app::screen_class_name{ L"screen window" };
 
@@ -81,16 +81,40 @@ LRESULT app::window_proc(HWND window, UINT message, WPARAM wparam, LPARAM lparam
 			if (sizeCount == 1)
 			{
 				change += 10;
-				SetWindowRgn(window, CreateEllipticRgn(0, 0, change, change), TRUE);
+				//SetWindowRgn(window, CreateEllipticRgn(0, 0, change, change), TRUE);
+				/*int centerX = (circleRect.left + circleRect.right) / 2;
+				int centerY = (circleRect.top + circleRect.bottom) / 2;
+				int left = centerX - change / 2;
+				int top = centerY - change / 2;
+				int right = centerX + change / 2;
+				int bottom = centerY + change / 2;
+				circleRect.left = left;
+				circleRect.right = right;
+				circleRect.top = top;
+				circleRect.bottom = bottom;
+
+				InvalidateRect(m_screen, NULL, TRUE);
+				UpdateWindow(m_screen);*/
+
 				if(change >= 100)
 					sizeCount = 0;
 			}
 			else if (sizeCount == 0)
 			{
 				change -= 10;
-				SetWindowRgn(window, CreateEllipticRgn(0, 0, change, change), TRUE);
-				if (change <= 50)
-					sizeCount = 1;
+				//SetWindowRgn(window, CreateEllipticRgn(0, 0, change, change), TRUE);
+				/*int centerX = (circleRect.left + circleRect.right) / 2;
+				int centerY = (circleRect.top + circleRect.bottom) / 2;
+				int left = centerX - change / 2;
+				int top = centerY - change / 2;
+				int right = centerX + change / 2;
+				int bottom = centerY + change / 2;
+				circleRect.left = left;
+				circleRect.right = right;
+				circleRect.top = top;
+				circleRect.bottom = bottom;
+				InvalidateRect(m_screen, NULL, TRUE);
+				UpdateWindow(m_screen);*/
 			
 			}
 			
@@ -99,7 +123,14 @@ LRESULT app::window_proc(HWND window, UINT message, WPARAM wparam, LPARAM lparam
 		{
 			POINT pos;
 			GetCursorPos(&pos);
-			SetWindowPos(m_main, NULL, pos.x - change / 4 , pos.y - change / 4, 0, 0, SWP_NOSIZE | SWP_NOZORDER);
+			RECT pastRect = circleRect;
+			circleRect.left = pos.x - 50;
+			circleRect.top = pos.y - 50;
+			circleRect.right = circleRect.left + 100;
+			circleRect.bottom = circleRect.top + 100;
+			InvalidateRect(m_screen, NULL, TRUE);
+			UpdateWindow(m_screen);
+			//SetWindowPos(m_main, NULL, pos.x - change / 4 , pos.y - change / 4, 0, 0, SWP_NOSIZE | SWP_NOZORDER);
 		}
 		break;
 		case WM_HOTKEY:
@@ -114,8 +145,21 @@ LRESULT app::window_proc(HWND window, UINT message, WPARAM wparam, LPARAM lparam
 					pulse = false;
 					ShowErrorMessageBox(0);
 					break;
+				case F12:
+					help = false;
+					break;
 
 			}
+			break;
+		case WM_PAINT: 
+			PAINTSTRUCT ps;
+			HDC hdc = BeginPaint(m_screen, &ps);
+			
+			SelectObject(hdc, hbrush);
+			Ellipse(hdc, circleRect.left, circleRect.top, circleRect.right, circleRect.bottom);
+			if(help)
+				DrawText(hdc, L"Pomoc ctrl f12 wylacz", -1, &textRect, DT_SINGLELINE | DT_CENTER | DT_VCENTER);
+			EndPaint(m_screen, &ps);
 			break;
 
 	}
@@ -132,6 +176,10 @@ HWND app::create_window(DWORD style)
 	sizeCount = 1;
 	change = 100;
 	pulse = true;
+	circleRect.left = x - 50;
+	circleRect.top = y - 50;
+	circleRect.right = circleRect.left + 100;
+	circleRect.bottom = circleRect.top + 100;
 	HWND window = CreateWindowExW(WS_EX_LAYERED | WS_EX_TOOLWINDOW | WS_EX_TRANSPARENT, s_class_name.c_str(), L"circle", style, x - 50 , y - 50 , 100, 100, nullptr, nullptr, m_instance, this);
 	//SetCapture(window);
 	SetTimer(window, ID_TIMER1, 5, NULL);
@@ -139,7 +187,7 @@ HWND app::create_window(DWORD style)
 	//RegisterHot();
 	SetLayeredWindowAttributes(window, 0, 255 * 80 / 100, LWA_ALPHA);
 	SetWindowPos(window, HWND_TOPMOST, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE);
-	SetWindowRgn(window, CreateEllipticRgn(0,0,100,100), TRUE);
+	//SetWindowRgn(window, CreateEllipticRgn(0,0,100,100), TRUE);
 
 	return window;
 }
@@ -154,6 +202,14 @@ void app::RegisterHot()
 		ShowErrorMessageBox(GetLastError());
 	}
 	if (RegisterHotKey(NULL, F4, MOD_ALT | MOD_SHIFT, VK_F4))
+	{
+		// Hotkey registered successfully
+	}
+	else
+	{
+		ShowErrorMessageBox(GetLastError());
+	}
+	if (RegisterHotKey(m_screen, F12, MOD_CONTROL, VK_F12))
 	{
 		// Hotkey registered successfully
 	}
@@ -190,23 +246,51 @@ void app::ShowErrorMessageBox(DWORD errorCode)
 	// Free the buffer allocated by FormatMessage
 	LocalFree(errorMsgBuffer);
 }
+void app::create_notify()
+{
+	nid.cbSize = sizeof(NOTIFYICONDATA);
+	nid.hWnd = NULL;
+	nid.uID = 1;
+	nid.uFlags = NIF_ICON | NIF_MESSAGE | NIF_TIP;
+	nid.uCallbackMessage = WM_USER + 1;
+	nid.hIcon = LoadIcon(m_instance, IDI_APPLICATION);
+	
+
+	Shell_NotifyIcon(NIM_ADD, &nid);
+}
 app::app(HINSTANCE instance) : m_instance{ instance }
 {
 	register_class();
 	registerScreen_class();
-	/*screenWindow sc{ instance };
-	screen = sc;
-	screen.register_class();
-	screen.create_screen_window();*/
 	DWORD main_style = WS_OVERLAPPED | WS_POPUP;
 	m_main = create_window(main_style);
 	create_screen_window();
+	help = true;
+	hbrush = CreateSolidBrush(RGB(255, 255, 0));
+
+}
+
+void app::chooseColor()
+{
+	ZeroMemory(&cc, sizeof(cc));
+	cc.lStructSize = sizeof(cc);
+	cc.hwndOwner = m_screen;
+	cc.lpCustColors = (LPDWORD)acrCustClr;
+	cc.rgbResult = rgbCurrent;
+	cc.Flags = CC_FULLOPEN | CC_RGBINIT;
+	if (ChooseColor(&cc) == TRUE)
+	{
+		hbrush = CreateSolidBrush(cc.rgbResult);
+		rgbCurrent = cc.rgbResult;
+	}
 }
 
 int app::run(int show_command)
 {
-	ShowWindow(m_main, show_command);
+	//ShowWindow(m_main, show_command);
 	ShowWindow(m_screen, show_command);
+	create_notify();
+	chooseColor();
 	MSG msg{};
 	BOOL result = TRUE;
 
@@ -220,6 +304,7 @@ int app::run(int show_command)
 
 
 	}
+	Shell_NotifyIcon(NIM_DELETE, &nid);
 	return EXIT_SUCCESS;
 }
 bool app::registerScreen_class()
--- lab1pige/app.h
+++ lab1pige/app.h
@@ -4,6 +4,7 @@
 #include <string>
 #include <ctime>
 #include "screenWindow.h"
+#include <shellapi.h>
 #define pocisk_count 5
 class app
 {
@@ -25,6 +26,17 @@ public:
 	static std::wstring const screen_class_name;
 	void create_screen_window();
 	HWND m_screen;
+	RECT circleRect;
+	RECT textRect = { 0,0,200,200 };
+	bool help;
+	
+	NOTIFYICONDATA nid;
+	void create_notify();
+	void chooseColor();
+	CHOOSECOLOR cc;                 
+	COLORREF acrCustClr[16];
+	HBRUSH hbrush;                  // brush handle
+	DWORD rgbCurrent;
 
 
 
